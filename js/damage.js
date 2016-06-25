ce929dd769a5e83a5bf131b3bfd129519ae22b79
bmeduho <bmeduho@users.noreply.github.com>
2016-06-25 18:11:46 -0400

    Update damage.js

--- js/damage.js
+++ js/damage.js
@@ -21,6 +21,9 @@ function CALCULATE_ALL_MOVES_BW(p1, p2, field) {
     p2.stats[SA] = getModifiedStat(p2.rawStats[SA], p2.boosts[SA]);
     var side1 = field.getSide(1);
     var side2 = field.getSide(0);
+    if (side1.inverse === "Inverse" || side2.inverse = "Inverse") {
+        typeChart = TYPE_CHART_INVERSE
+    }
     checkInfiltrator(p1, side1);
     checkInfiltrator(p2, side2);
     var results = [[],[]];
